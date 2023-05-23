d8e3514515232f08531b1ae035e116100c2ecaf3
Erkan <erkankolakan@gmail.com>
2023-05-23 19:38:10 +0300

    veri_cekildi

--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
-import React from 'react';
+import React, {useState , useEffect} from 'react';
+import axios from 'axios';
+
 
 
 function App() {
+
+  const [veri, setVeri] = useState();
+  const [tarih , setTarih] = useState("");
+
+  useEffect( () => {
+    axios.get("https://raw.githubusercontent.com/ozanerturk/covid19-turkey-api/master/dataset/timeline.json")
+    .then(res => setVeri(res.data[tarih]))
+    .catch(err => console.log(err))
+
+
+
+  },[veri,tarih]) 
+
+
+
 
   return (
     <div className="App">
       <div className='container'>
         <div className='ro'>
           <div className='col-md-8 mx-auto mt-4'>
-            <h1 className='text-center text-white display-3'>TÜRKİYE COVİD-19 Arama Motoru</h1>
-            <input type="text" className='form-control' placeholder='GG/AA/YY'/>
-            <table class="table table-striped text-white">
+            <h1 className='text-center text-white display-3'>TÜRKİYE <span Style="color:red"> COVİD-19 </span> Arama Motoru</h1>
+            <input type="text" className='form-control' placeholder='GG/AA/YY'
+            onChange={(e) => setTarih(e.target.value) }
+            
+            />
+            <table className="table table-striped text-white">
   <thead>
           <tr>
             <th scope="col">#</th>
@@ -19,12 +39,12 @@ function App() {
             <th scope="col">Vefat Sayısı</th>
           </tr>
         </thead>
-        <tbody>
-          <tr>
-            <th scope="row">1</th>
-            <td>13600</td>
-            <td>2500</td>
-            <td>20</td>
+        <tbody >
+          <tr >
+            <th scope="row" >1</th>
+            <td>{veri === undefined ? "Veri Bekleniyor" : veri.totalTests}</td>
+            <td>{veri === undefined ? "Veri Bekleniyor" : veri.patients}</td>
+            <td>{veri === undefined ? "Veri Bekleniyor" : veri.deaths}</td>
           </tr>
         </tbody>
       </table>
