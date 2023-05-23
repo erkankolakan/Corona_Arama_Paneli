8347aa4cb17c7678826a6ab45d06a05e01e3a339
Erkan <erkankolakan@gmail.com>
2023-05-23 18:30:09 +0300

    Covid_Arama_Paneli

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='container'>
+        <div className='ro'>
+          <div className='col-md-8 mx-auto mt-4'>
+            <h1 className='text-center text-white display-3'>TÜRKİYE COVİD-19 Arama Motoru</h1>
+            <input type="text" className='form-control' placeholder='GG/AA/YY'/>
+            <table class="table table-striped text-white">
+  <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Test Sayısı</th>
+            <th scope="col">Hasta Sayısı</th>
+            <th scope="col">Vefat Sayısı</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td>13600</td>
+            <td>2500</td>
+            <td>20</td>
+          </tr>
+        </tbody>
+      </table>
+          </div>
+        </div>
+      </div>
     </div>
-  );
-}
-
+  ); 
+} 
 export default App;
